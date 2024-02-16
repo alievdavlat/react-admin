d47bc9bb39f6c115fb27c2454f305da800740605
aliev <122043094+alievdavlat@users.noreply.github.com>
2024-02-16 10:33:59 +0500

    done

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,14 @@ import './assets/style/normalize.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Provider store={store}>
-      <App />
+          <ToastContainer />
+           <App />
       </Provider>
     </BrowserRouter>
 )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { InputRegular2 } from '../components'
+
 
 const Login = () => {
   return (
-    <div>Login</div>
+
+    <div className='login'>
+
+    <form className='login-form'>
+      <div className='login-form-title'>
+          <h4>Login</h4>
+      </div>
+
+      <div className="input-box">
+      <InputRegular2/>
+      <InputRegular2/>
+      </div>
+
+
+    </form>
+
+    </div>
   )
 }
 
